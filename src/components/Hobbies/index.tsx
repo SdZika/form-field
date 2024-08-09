8750b357703428e7e90192fe9d3dab7f5d12bb97
SdZika <markozivkovic1982@gmail.com>
2024-08-09 14:23:03 +0200

    add hobby function

--- src/components/Hobbies/index.tsx
+++ src/components/Hobbies/index.tsx
@@ -1,17 +1,32 @@
-import React, { FC, useState } from 'react'
+import React, { ChangeEvent, FC, useState } from 'react'
 
 interface PropsHobbies {
     id?: number;
     title: string;
+    
 }
 
 export const Hobbies:FC = () => {
 
+    
     const [hobbies, setHobbies] = useState<PropsHobbies[]>([
         { id: 1, title: 'Reading' },
         { id: 2, title: 'Gaming' },
         { id: 3, title: 'Cooking' }
     ])
+
+    const [title, setTitle] = useState<string>('')
+
+    const addTitle = (e: ChangeEvent<HTMLInputElement>) => {
+        setTitle(e.target.value)
+    }
+
+
+
+  const updateHobbies = () => {
+    setHobbies(previousHobbies => ([...previousHobbies, {id: previousHobbies.length + 1, title: title}]))}
+
+   
 
   return (
     <div>
@@ -20,6 +35,9 @@ export const Hobbies:FC = () => {
                 <p key={hobby.id}>{hobby.title}</p>
             ))
         }
+
+        <input type='text' value={title} placeholder='Add Hobby' onChange={addTitle}></input>
+        <button onClick={updateHobbies}>Add Hobby</button>
         
     </div>
   )
